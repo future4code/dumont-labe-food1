c1c1fcb1bd84213136be9bb55b3f5097b7d5472a
Michael Soto <michael_nickolas@hotmail.com>
2020-12-15 11:49:41 -0300

    entregaProjeto

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
-
+import HomeScreen from "./screens/HomeScreen/HomeScreen"
+// import Feed from './screens/Feed/Feed'
 import Router from "./routes/Router";
 
 function App() {
   return (
     <div className="App">
+      <HomeScreen/>
       <Router />
     </div>
   );
